b8f015e170c178576102a6a0f759ccb4b3d36a66
arvrao <arvindrao.759@gmail.com>
2023-08-07 22:56:11 +0530

    Added complete clerk authentication to the application

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-    return ( 
-        <div>
-            <div className='text-5xl text-gray-600 font-light'>ChatVerse AI</div>
-            Login page (unprotected)</div>
-     );
-}
- 
-export default LoginPage;
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,8 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 const RootPage = () => {
     return ( 
         <div>
             Root page (protected)
             <div className='text-5xl text-gray-600 font-light'>ChatVerse AI</div>
+            <UserButton afterSignOutUrl="/" />
         </div>
      );
 }
