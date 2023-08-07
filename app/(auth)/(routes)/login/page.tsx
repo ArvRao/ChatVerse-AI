687744ae4007179d9179ff6b93bcb1da8ed68ac7
arvrao <arvindrao.759@gmail.com>
2023-08-07 22:15:13 +0530

    Initial folder setup with route groups

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return ( 
+        <div>
+            <div className='text-5xl text-gray-600 font-light'>ChatVerse AI</div>
+            Login page (unprotected)</div>
+     );
+}
+ 
+export default LoginPage;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+const RootPage = () => {
+    return ( 
+        <div>
+            Root page (protected)
+            <div className='text-5xl text-gray-600 font-light'>ChatVerse AI</div>
+        </div>
+     );
+}
+ 
+export default RootPage;
