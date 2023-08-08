8dd64dceef19797745e7589a26be608c2e3f92fa
arvrao <arvindrao.759@gmail.com>
2023-08-08 23:53:49 +0530

    Added home page and made changes in the auth middleware to make default public route

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 const RootPage = () => {
     return ( 
         <div>
-            Root page (protected)
             <div className='text-5xl text-gray-600 font-light'>ChatVerse AI</div>
-            <UserButton afterSignOutUrl="/" />
+            <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" href='/home'>Get started</Link>
         </div>
      );
 }
- 
+
 export default RootPage;
