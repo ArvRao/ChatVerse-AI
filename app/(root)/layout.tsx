e84b423f6bf6a054d962c14b2fb4fd98d7fa4329
arvrao <arvindrao.759@gmail.com>
2023-08-11 23:59:16 +0530

    sidebar updates

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,15 @@
+import { Sidebar } from "@/components/sidebar";
+
 const RootLayout = ({
     children
 }: {
     children: React.ReactNode
 }) => {
-    return ( 
+    return (
         <div className="h-full">
+            <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-1">
+                <Sidebar />
+            </div>
                 {children}
         </div>
      );
