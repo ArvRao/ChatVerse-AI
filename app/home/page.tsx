f79d06ae8fc9b973c8555dc4be4f18f6eea5682b
arvrao <arvindrao.759@gmail.com>
2023-08-13 23:24:40 +0530

    Added search bar

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,11 @@
+import SearchInput from "@/components/search-input";
+
 const Home = () => {
     return ( 
-        <div className="p-10 text-4xl">
+        <div className="pl-10 pt-2 text-4xl">
             Welcome to ChatVerse-AI homepage
             <div className="p-5 text-xl">
+                <SearchInput /> <br />
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus eos non omnis animi ea veritatis deserunt excepturi. Accusantium molestias totam animi quaerat unde ea adipisci sequi nesciunt, beatae aut! Excepturi!
             </div>
         </div>
---
+++ components/search-input.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
+export const SearchInput = () => {
+    return ( 
+        <div className="relative">
+            <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+            <Input 
+            placeholder="Search..."
+            className="pl-10 bg-primary/10 w-80"
+            />
+        </div>
+     );
+}
+ 
+export default SearchInput;
