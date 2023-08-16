4cffab7bfad142644fa89426243be3fbe3c70c03
arvrao <arvindrao.759@gmail.com>
2023-08-16 22:27:30 +0530

    Updated contacts page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,19 @@
 const Contact = () => {
     return ( 
-        <div className="text-lg p-7">
-            Hey there. Im Arvind and the creator of ChatVerse-AI.
-            I do hope you love it as much as I do.
-            Feel free to connect with me.
-            Social media links
+        <div className="text-lg p-10 pt-15">
+            {/* Hey there. Im Arvind and the creator of ChatVerse-AI.
+            I do hope you find this application useful and fun.
+            I&ldquo;m more than keen to know about your thoughts and open to any feedbacks.
+            Please feel free to connect with me */}
+            
+            Greetings! Im Arvind, a software engineer with a profound enthusiasm for delving deeper into the realms of Machine Learning and Artificial Intelligence. Ive  crafted this application that brings users the exhilarating opportunity to engage in lifelike conversations with renowned real-world personalities. <br />
+
+    Ive poured my heart and soul into creating an experience that seamlessly bridges the virtual world with reality, offering users a unique chance to interact with their favorite stars. Your thoughts and feedback are immensely valuable to me as I continuously strive to enhance both the utility and enjoyment factor of this application.
+
+    I wholeheartedly invite you to explore this creation and would greatly appreciate any insights and feedback you might have to offer. Im excited to connect and discuss how we can elevate this experience together.
+    React out to me at:
+    Email: <a href="arvindrao.759@gmail.com">arvindrao.759@gmail.com</a>
+
         </div>
      );
 }
