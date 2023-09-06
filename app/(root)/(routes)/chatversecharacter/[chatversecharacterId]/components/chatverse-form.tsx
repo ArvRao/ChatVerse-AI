280337c182b36d616b49e61fdb9c874c0f02bc6c
arvrao <arvindrao.759@gmail.com>
2023-09-06 20:35:45 +0530

    The create AI character UI

--- app/(root)/(routes)/chatversecharacter/[chatversecharacterId]/components/chatverse-form.tsx
+++ app/(root)/(routes)/chatversecharacter/[chatversecharacterId]/components/chatverse-form.tsx
@@ -1,13 +1,45 @@
 "use client";
 
+import { Wand2 } from "lucide-react";
 import * as z from "zod";
 import { Category, ChatVerseCharacter } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormField, FormItem, FormControl, FormMessage, FormLabel } from "@/components/ui/form";
+import { Form,
+     FormField, 
+     FormItem, 
+     FormControl, 
+     FormDescription, 
+     FormMessage, 
+     FormLabel 
+    } from "@/components/ui/form";
 import { Separator } from "@/components/ui/separator";
 import { ImageUpload } from "@/components/image-upload";
 import { Input } from "@/components/ui/input";
+import { Select, 
+    SelectTrigger, 
+    SelectValue, 
+    SelectContent, 
+    SelectItem 
+    } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
+const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
+`;
+
+const SEED_CHAT = `Human: Hi Elon, how's your day been?
+Elon: Busy as always. Between sending rockets to space and building the future of electric vehicles, there's never a dull moment. How about you?
+
+Human: Just a regular day for me. How's the progress with Mars colonization?
+Elon: We're making strides! Our goal is to make life multi-planetary. Mars is the next logical step. The challenges are immense, but the potential is even greater.
+
+Human: That sounds incredibly ambitious. Are electric vehicles part of this big picture?
+Elon: Absolutely! Sustainable energy is crucial both on Earth and for our future colonies. Electric vehicles, like those from Tesla, are just the beginning. We're not just changing the way we drive; we're changing the way we live.
+
+Human: It's fascinating to see your vision unfold. Any new projects or innovations you're excited about?
+Elon: Always! But right now, I'm particularly excited about Neuralink. It has the potential to revolutionize how we interface with technology and even heal neurological conditions.
+`;
 
 interface ChatverseIdPageProps {
     initialData: ChatVerseCharacter | null;
@@ -30,7 +62,7 @@ const formSchema = z.object({
     src: z.string().min(1, {
         message: "Image is required.",
     }),
-    categoryId: z.string().min(200, {
+    categoryId: z.string().min(1, {
         message: "Category is required.",
     }),
 })
@@ -86,7 +118,7 @@ export const ChatverseForm = ({
                     )}
                     />
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <FormField
+                    <FormField
                         name="name"
                         control={form.control}
                         render={({ field }) => (
@@ -99,9 +131,120 @@ export const ChatverseForm = ({
                                         {...field}
                                     />
                                 </FormControl>
+                                <FormDescription>
+                                    This is how your ChatVerse AI companion will be named.
+                                </FormDescription>
+                                <FormMessage />
                             </FormItem>
                         )}
-                        ></FormField>
+                        />
+                    <FormField
+                        name="description"
+                        control={form.control}
+                        render={({ field }) => (
+                            <FormItem className="col-span-2 md:col-span-1">
+                                <FormLabel>Description</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        disabled={isLoading}
+                                        placeholder="CEO of Tesla, SpaceX and many more..."
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormDescription>
+                                    Short description for your AI character
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                         <FormField
+              control={form.control}
+              name="categoryId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <Select disabled={isLoading} onValueChange={field.onChange} value={field.value} defaultValue={field.value}>
+                    <FormControl>
+                      <SelectTrigger className="bg-background">
+                        <SelectValue defaultValue={field.value} placeholder="Select a category" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {categories.map((category) => (
+                        <SelectItem key={category.id} value={category.id}>{category.name}</SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormDescription>
+                    Select a category for your AI
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+                    </div>
+                    <div className="space-y-2 w-full">
+                        <div>
+                            <h3 className="text-lg font-medium">
+                                Configuration
+                            </h3>
+                            <p className="text-sm text-muted-foreground">
+                                Detailed instructions for our AI behaviour
+                            </p>
+                        </div>
+                        <Separator className="bg-primary/10" />
+                    </div>
+                    <FormField
+                        name="instructions"
+                        control={form.control}
+                        render={({ field }) => (
+                            <FormItem className="col-span-2 md:col-span-1">
+                                <FormLabel>Instructions</FormLabel>
+                                <FormControl>
+                                    <Textarea
+                                    className="bg-background resize-none"
+                                    rows={7}
+                                        disabled={isLoading}
+                                        placeholder={PREAMBLE}
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormDescription>
+                                    Describe in detail you Chatverse AI&apos;s backstory & relevant details.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        name="seed"
+                        control={form.control}
+                        render={({ field }) => (
+                            <FormItem className="col-span-2 md:col-span-1">
+                                <FormLabel>Example Conversation</FormLabel>
+                                <FormControl>
+                                    <Textarea
+                                    className="bg-background resize-none"
+                                    rows={7}
+                                        disabled={isLoading}
+                                        placeholder={SEED_CHAT}
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormDescription>
+                                    Describe in detail you Chatverse AI&apos;s backstory & relevant details.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <div className="w-full flex justify-center">
+                        <Button size="lg" disabled={isLoading}>
+                            {initialData ? "Edit your AI Character" : "Create your companion"}
+                            <Wand2 className="w-4 h-4 ml-2" />
+                        </Button>
+
                     </div>
                 </form>
             </Form>
